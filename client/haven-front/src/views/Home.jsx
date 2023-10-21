81dbca96d5c5ac37816d10b2aa5a26e26f28f513
*arrais* <134543499+rodriguesarrais@users.noreply.github.com>
2023-10-21 20:32:57 -0300

    setando lógica de criar posts

--- client/haven-front/src/views/Home.jsx
+++ client/haven-front/src/views/Home.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import axios from 'axios'
 
 function Home() {
+
+  async function fetchPosts() {
+    const posts = await axios.get("/api/posts");
+  }
+
+
+
+  const createPost = () => {
+    alert("post criado");
+    
+  };
+
   return (
     <div>
-      <Navbar/>
+      <h1>Teste de posts</h1>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <br />
+      <button onClick={createPost}>Criar Post</button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
